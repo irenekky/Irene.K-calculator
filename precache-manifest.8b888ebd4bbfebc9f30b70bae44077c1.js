ede8ab85ded7d4124e716d4a50b4dee64a2414d0
Irene Kilonzo <irenedunge@yahoo.com>
2020-09-20 07:04:04 +0300

    Updates

---
+++ precache-manifest.8b888ebd4bbfebc9f30b70bae44077c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a697ac6f23a4b1ae6ee12d91a4b339",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "55e43901dfeb9429761a",
+    "url": "/react-gh-pages/static/css/main.cbc24d4f.chunk.css"
+  },
+  {
+    "revision": "9fe772f1d191b7a379a3",
+    "url": "/react-gh-pages/static/js/2.91e36bf5.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/react-gh-pages/static/js/2.91e36bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e43901dfeb9429761a",
+    "url": "/react-gh-pages/static/js/main.6792d726.chunk.js"
+  },
+  {
+    "revision": "7c8bb89b2cc8912dfb47",
+    "url": "/react-gh-pages/static/js/runtime-main.a6262974.js"
+  }
+]);
